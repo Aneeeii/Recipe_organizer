fb7e238cd6d7f1223780ac09603b675e41e95159
David Lim <ddavidd.lim@gmail.com>
2026-04-15 18:33:38 -0700

    feat: almost done with navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,99 @@
-import { AppBar, Toolbar } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, Button, Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import InputBase from "@mui/material/InputBase";
+import { alpha, styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
 
 export default function Navbar() {
-    return (
-        <AppBar position="static" sx={navbarStyles.appbar}>
-            <Toolbar>
-                Recipes
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="static" sx={navbarStyles.appbar}>
+      <Toolbar>
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography>Recipes</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 3,
+            }}
+          >
+            <Button variant="text">Home</Button>
+            <Button variant="text">Discover</Button>
+            <Button variant="text">Add</Button>
+            
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
-
 
 const navbarStyles = {
-    appbar: {
-        margin: 0,
-        borderRadius: 0,
-        padding: 0,
-        backgroundColor: "#798262"
+  appbar: {
+    margin: 0,
+    borderRadius: 0,
+    padding: 0,
+    backgroundColor: "#798262",
+  },
+};
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
     },
-}
+  },
+}));
